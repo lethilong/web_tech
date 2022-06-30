8cdf8e0818d5feaab3f58450f14f0956c6d319f9
Doan Son <Sown.211@gmail.com>
2022-06-30 21:02:20 +0700

    son

---
+++ application/controllers/product.php
@@ -0,0 +1,20 @@
+<?php
+class Product extends Controller {
+  private $productModel;
+
+  public function __construct() {
+    $this->productModel = $this->load_model("Product");
+  }
+
+  public function index() {
+    $data['page-title'] = 'Products';
+    $data['products'] = $this->productModel->get_all();
+    this->view('product', $data);
+  }
+
+  public function create() {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      //...
+    }
+  }
+}
