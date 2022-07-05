8d3cf230c792bfe35908bc33b234218cebb206e0
Doan Son <doanson2k1@gmail.com>
2022-07-05 09:34:08 +0700

    update product

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -8,13 +8,19 @@ class Product extends Controller {
 
   public function index() {
     $data['page-title'] = 'Products';
-    $data['products'] = $this->productModel->get_all();
+    $data['products'] = $this->productModel->getAllProds();
     this->view('products', $data);
   }
 
   public function create() {
+    $data['page-title'] = 'Add Product';
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      //...
+      if ($this->productModel->create($_POST, $_FILES)) {
+        Redirect::to(products);
+      } else {
+        //$data['products'] = $this->productModel->getAllProd
+        $this->view('products.add', $data);
+      }
     }
   }
 }
